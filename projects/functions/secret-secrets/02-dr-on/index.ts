348469e0ddde0c3e35fde8956802ab66f245f6b4
Matilda Mared <matildamared@live.se>
2022-09-19 10:04:54 +0200

    add solution to functions/secret secrets/dr on :q :quit

--- projects/functions/secret-secrets/02-dr-on/index.ts
+++ projects/functions/secret-secrets/02-dr-on/index.ts
@@ -1,2 +1,28 @@
 // Write your createAdvancedCipher function here! ✨
 // You'll need to export it so the tests can run it.
+
+type OnVowel = (vowel: string) => string;
+type OnConsonant = (consonant: string) => string;
+type OnPunctuation = (punctuation: string) => string;
+
+export function createAdvancedCipher(
+	onVowel: OnVowel,
+	onConsonant: OnConsonant,
+	onPunctuation: OnPunctuation
+): (text: string) => string {
+	return (text: string) => {
+		let result = "";
+
+		for (let character of text) {
+			if (character.match(/[aeiou]/i)) {
+				result += onVowel(character);
+			} else if (character.match(/[bcdfghjklmnpqrstvwxyz]/i)) {
+				result += onConsonant(character);
+			} else {
+				result += onPunctuation(character);
+			}
+		}
+
+		return result;
+	};
+}
