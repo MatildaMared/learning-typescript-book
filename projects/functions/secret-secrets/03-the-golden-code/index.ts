bdcce78e907c1844898e73e24f225877b5ea4b5c
Matilda Mared <matildamared@live.se>
2022-09-19 10:15:55 +0200

    add solution to functions/secret secrets/the golden code

--- projects/functions/secret-secrets/03-the-golden-code/index.ts
+++ projects/functions/secret-secrets/03-the-golden-code/index.ts
@@ -1,2 +1,22 @@
 // Write your createCodeCracker function here! ✨
 // You'll need to export it so the tests can run it.
+
+type Options = {
+	attempts: number;
+	makeGuess: (text: string, attempt: number) => string;
+	validateGuess: (guess: string) => boolean;
+};
+
+export function createCodeCracker(
+	options: Options
+): (text: string) => string | undefined {
+	return (text: string) => {
+		for (let attempt = 0; attempt < options.attempts; attempt++) {
+			const guess = options.makeGuess(text, attempt);
+			if (options.validateGuess(guess)) {
+				return guess;
+			}
+		}
+		return undefined;
+	};
+}
