7f46bb47f6d2b0a7d3464dcb0698244504db8f1c
Matilda Mared <matildamared@live.se>
2022-09-19 09:58:36 +0200

    add solution for functions/secret secrets/incoming cipher

--- projects/functions/secret-secrets/01-incoming-cipher/index.ts
+++ projects/functions/secret-secrets/01-incoming-cipher/index.ts
@@ -1,2 +1,14 @@
 // Write your createCipher function here! ✨
 // You'll need to export it so the tests can run it.
+
+export function createCipher(
+	cipher: (text: string) => string
+): (text: string) => string {
+	return (text: string) => {
+		let result = "";
+		for (let i = 0; i < text.length; i++) {
+			result += cipher(text[i]);
+		}
+		return result;
+	};
+}
